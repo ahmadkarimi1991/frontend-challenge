a2c97b64ff05379d8ab849be3b32c52d4d08ee95
Ahmad <ahmadkarimi1991@gmail.com>
2021-07-09 00:53:51 +0430

    Separate development and production config files and add serve script

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+  entry: {
+    app: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
---
+++ webpack.dev.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+});
